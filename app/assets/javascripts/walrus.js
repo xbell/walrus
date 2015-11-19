695495cdad70243a155de999b0d9941bf9a66934
Evelyn <evieb16@gmail.com>
2015-11-18 16:37:05 -0800

    Added underline feature when hovering over show/hide items link and made cursor into pointer when hovering

--- app/assets/javascripts/walrus.js
+++ app/assets/javascripts/walrus.js
@@ -2,14 +2,19 @@ $(document).ready(function() {
 
 
 $(".errors").click(function() {
-   $(".popup").hide() ; })
+   $(".popup").hide() ; });
 
 
 $(".toggle").click(function() {
   $(".toggle").toggle().css("color", "#95b9e3");
   $(".hide").toggle();
- })
+});
 
+$(".toggle").hover(function() {
+  $(this).css("text-decoration", "underline");
+}, function() {
+  $(this).css("text-decoration", "none");
+});
 
 
 
